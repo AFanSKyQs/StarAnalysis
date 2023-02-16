7a8b78cbc503df8989b3a08d9d04fdda8881cc68
FanSky_Qs <3141865879@qq.com>
2023-02-17 02:03:49 +0800

    默认主页

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+    <nuxt />
+</template>
+
+<script>
+  export default {
+    components: {
+    },
+    head(){
+      return {
+        title: "星探 - Web端APK在线分析系统"
+      }
+    },
+    mounted(){
+      this.$nextTick(() => {
+        this.$nuxt.$loading.start()
+        setTimeout(() => this.$nuxt.$loading.finish(), 500)
+      })
+    }
+  }
+</script>
+
+<style>
+</style>
